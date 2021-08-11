2cedee133b643b5fa3299738ea5ed9d4fdb0642e
rubyspch <ruby_chohan@hotmail.co.uk>
2021-08-11 20:02:55 +0200

    Initial commit

---
+++ Javascript/resistor-color-duo.js
@@ -0,0 +1,9 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (array) => {
+  return parseFloat(`${colours.indexOf(array[0])}${colours.indexOf(array[1])}`);
+};
+const colours = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
